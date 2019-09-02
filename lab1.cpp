e18230f9e1476343cd86f1d7db829448399db967
Catherine Leung <catherine.leung>
2019-09-02 04:00:48 -0400

    lab1 starter file and tester

---
+++ lab1.cpp
@@ -0,0 +1,19 @@
+/**************************************************/
+/*                                                */
+/*  Lab 1 Starter file                            */
+/*                                                */
+/*  Name:                                         */
+/*  Student number:                               */
+/*                                                */
+/**************************************************/
+
+unsigned int factorial (unsigned int n){
+
+}
+
+double power (double base, unsigned int n){
+
+}
+unsigned int fibonacci (unsigned int n){
+	
+}
