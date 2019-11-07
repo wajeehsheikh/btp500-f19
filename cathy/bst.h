331027b7cae55299428660ef0319678475c92e15
cathyatseneca <cathyatseneca@gmail.com>
2019-11-07 15:55:37 -0500

    bst main and updated starter

--- cathy/bst.h
+++ cathy/bst.h
@@ -1,6 +1,19 @@
 template <typename T>
 class BST{
 
+	struct Node{
+		T data_;
+		Node* left_;
+		Node* right_;
+		Node(const T& data=T{},Node* left=nullptr, Node* right=nullptr){
+			data_=data;
+			left_=left;
+			right_=right;
+		}
+	};
+	
+	Node* root_;
+
 public:
 	BST();
 	void insert(const T& data);
